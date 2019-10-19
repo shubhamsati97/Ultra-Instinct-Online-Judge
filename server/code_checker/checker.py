3c3e66ec318dde7b5b93ba8a9367daf06790ae2a
Shubham Sati <shubhamsati7991@gmail.com>
2019-10-20 02:27:08 +0530

    Added checker main file and the design idea

---
+++ server/code_checker/checker.py
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+# Determining the language for the file to be tested
+
+
+# Calling the respective helper functions
+
+
+#Checking the performance parameters of file --- time/memory/ etc
+
+
+#Returning the output as dictionary
